b0601d7eaddcee1dd02c67f6a23aa497beffefcd
solita-jasona <jason.ali@solita.fi>
2022-06-10 12:35:11 +0300

    add login view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
 const routes = [
   {
     path: '/',
@@ -14,7 +13,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component:  () => import(/* webpackChunkName: "logi " */ '../views/LoginView.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <div class="form-container">
+      <form @submit.prevent>
+        <input type="text" @input="username = $event.target.value" placeholder="username" class="form-field"/>
+        <input type="password" @input="password = $event.target.value" placeholder="password" class="form-field"/>
+        <button @click="login">login</button>
+      </form>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  data() {
+    return {  
+      username: "",
+      password: ""
+    };
+  },
+  async mounted() {
+    // const self = this;
+
+  },
+  methods: {
+    async login() {
+      const self = this;
+      if (!self.username || !self.password) {
+        alert("Fill all fields");
+        return false;
+      }
+      var login = await self.$store.dispatch("app/login", {username: self.username, password: self.password});
+      if (login == true) {
+        self.$router.push("/");
+      }
+      else {
+          alert(login);
+      }
+    }
+  }
+}
+</script>
