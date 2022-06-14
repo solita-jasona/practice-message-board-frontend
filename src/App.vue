1dd721d3579973e6d93e9c379faa35744b8b8b99
solita-jasona <jason.ali@solita.fi>
2022-06-14 12:41:50 +0300

    change margins and padding

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  min-height: 100vh;
+  background-color: #ffffff
+}
+
+.page-content {
+  padding: 0px 5px 0px 5px;
 }
 
 nav {
@@ -57,5 +63,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+body {
+  margin: 0px;
 }
 </style>
