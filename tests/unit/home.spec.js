6158c4b786770b7e6b5db376a7c07286cbbbcd47
solita-jasona <jason.ali@solita.fi>
2022-06-30 16:46:22 +0300

    add some tests

---
+++ tests/unit/home.spec.js
@@ -0,0 +1,75 @@
+import { mount } from '@vue/test-utils'
+import Home from '@/views/HomeView.vue'
+import { createStore } from 'vuex'
+
+const app = {
+  namespaced: true,
+  state: {
+    user: null,
+    topics: null,
+    currentTopic: null,
+    messages: null
+  },
+  mutations: {
+    updateUser(state, user) {
+      state.user = user;
+    },
+    updateTopics(state, topics) {
+      state.topics = topics;
+    },
+    updateCurrentTopic(state, topic) {
+      state.currentTopic = topic;
+    },
+    updateMessages(state, messages) {
+      state.messages = messages;
+    }
+  },
+  actions: {
+    async getTopics(context) {
+      var data = [
+        {
+          id: 4003,
+          lastMessageTimeStamp: "2022-06-29T14:10:06.81592",
+          messageCount: 7,
+          title: "test 2",
+          userId: 2,
+        }
+      ];
+      context.commit("updateTopics", data)
+     
+    }
+  }
+};
+const store = createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    app
+  }
+})
+
+describe('HomeView.vue', () => {
+    it('renders correct title', () => {
+      const wrapper = mount(Home, {
+        global: {
+          plugins: [store]
+        }
+      })
+      const title = "Topics";
+      expect(wrapper.find('h1').text()).toEqual(title)
+    })
+
+    it('topics visible', () => {
+      const wrapper = mount(Home, {
+        global: {
+          plugins: [store]
+        }
+      })
+      const topic = "test 2";
+      expect(wrapper.find('.topics-container .topic:nth-of-type(1) .title-container span').text()).toEqual(topic)
+    })
+})
---
+++ tests/unit/register.spec.js
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+import Register from '@/views/RegisterView.vue'
+
+describe('RegisterView.vue', () => {
+    it('renders correct title', () => {
+      const wrapper = mount(Register)
+      const title = "Register";
+      expect(wrapper.find('.title').text()).toEqual(title)
+    })
+    it('validates good email', () => {
+        const wrapper = mount(Register);
+        const email = "test@gmail.com";
+        expect(wrapper.vm.emailIsValid(email)).toBeTruthy();
+    }) 
+    it('validates bad email', () => {
+        const wrapper = mount(Register);
+        const email = "tes.t.gmail.com";
+        expect(wrapper.vm.emailIsValid(email)).toBeFalsy();
+    })
+    it('validates matching passwords', () => {
+        const wrapper = mount(Register);
+        const pass1 = "testpassword";
+        const pass2 = "testpassword";
+        console.log("wrapper", wrapper);
+        expect(wrapper.vm.passwordsMatch(pass1,pass2)).toBeTruthy();
+    })
+    it('validates non-matching passwords', () => {
+        const wrapper = mount(Register);
+        const pass1 = "testpassword";
+        const pass2 = "differentpassword";
+        console.log("wrapper", wrapper);
+        expect(wrapper.vm.passwordsMatch(pass1,pass2)).toBeFalsy();
+    })
+    it('validates missing password', () => {
+        const wrapper = mount(Register);
+        const pass1 = "testpassword";
+        const pass2 = "";
+        console.log("wrapper", wrapper);
+        expect(wrapper.vm.passwordsMatch(pass1,pass2)).toBeFalsy();
+    })
+})
